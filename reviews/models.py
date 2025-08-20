bc8f208d44ad57c8cf28da0c051e5ef1e8db75f5
Vivek Dhengale <vivekdhengale84@gmail.com>
2025-08-20 13:06:31 +0530

    Added a database to store the Reviews of the users

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ReviewModel(models.Model):
+    username = models.CharField(max_length=100)
+    review_text = models.TextField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return f"Name: {self.username} Rating: {self.rating}"
