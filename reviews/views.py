63c9579121c6ac8ca841ef0ff507008160067aef
Vivek Dhengale <vivekdhengale84@gmail.com>
2025-08-20 12:03:19 +0530

    Added styling to the reviews form

--- reviews/views.py
+++ reviews/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 # Create your views here.
 
+from .forms import ReviewForm
+
 def review(request):
     if request.method == "POST":
-        entered_username = request.POST['username']
-        print(entered_username)
-        return HttpResponseRedirect("thank-you")
-    return render(request,"reviews/review.html")
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("thank-you")
+    else:
+        form = ReviewForm()
+    return render(request,"reviews/review.html",{
+        "form" : form
+    })
 
 def thank_you(request):
     return render(request,"reviews/thank_you.html")
