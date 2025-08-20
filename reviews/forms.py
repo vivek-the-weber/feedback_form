0df55dee1ddb305de2c4fe00244064cf93474b5d
Vivek Dhengale <vivekdhengale84@gmail.com>
2025-08-20 14:22:21 +0530

    Added model form that directly connects the models with the form also impleted classbased view inherited from class View

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,10 +1,27 @@
 from django import forms
 
-class ReviewForm(forms.Form):
-    username = forms.CharField(label="Your Name:",max_length=100,error_messages={
-        "required": "Your name must not be empty",
-        "max_length" : "Please enter small name"
-    })
-    review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea,max_length=200)
-    rating = forms.IntegerField(label="Your Rating", min_value=1,max_value=5)
-    
+from .models import ReviewModel
+# class ReviewForm(forms.Form):
+#     username = forms.CharField(label="Your Name:",max_length=100,error_messages={
+#         "required": "Your name must not be empty",
+#         "max_length" : "Please enter small name"
+#     })
+#     review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea,max_length=200)
+#     rating = forms.IntegerField(label="Your Rating", min_value=1,max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = ReviewModel
+        fields = "__all__"
+        # exclude = ["owner_comment"] 
+        labels = {
+            "username": "Your Name:",
+            "review_text":"Your Feedback:",
+            "rating":"Rating",
+        }
+        error_messages = {
+            "username": {
+                "required": "Your name must not be empty",
+                "max_length" : "Please enter small name"
+            }
+        }
--- reviews/views.py
+++ reviews/views.py
@@ -1,20 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import View
 # Create your views here.
+
 
 from .forms import ReviewForm
 
-def review(request):
-    if request.method == "POST":
+class ReviewView(View):
+    def get(self,request):
+        form = ReviewForm()
+
+        return render(request,"reviews/review.html",{
+        "form" : form
+        })
+    def post(self,request):
         form = ReviewForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            form.save()
             return HttpResponseRedirect("thank-you")
-    else:
-        form = ReviewForm()
-    return render(request,"reviews/review.html",{
+        return render(request,"reviews/review.html",{
         "form" : form
-    })
+        })
+
+# def review(request):
+#     if request.method == "POST":
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect("thank-you")
+#     else:
+#         form = ReviewForm()
+#     return render(request,"reviews/review.html",{
+#         "form" : form
+#     })
 
 def thank_you(request):
     return render(request,"reviews/thank_you.html")
