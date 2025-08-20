102ac989828663b84aa8bdaab7426814c018a80b
Vivek Dhengale <vivekdhengale84@gmail.com>
2025-08-20 09:19:18 +0530

    Initial Commit

---
+++ reviews/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+# Create your views here.
+
+def review(request):
+    if request.method == "POST":
+        entered_username = request.POST['username']
+        print(entered_username)
+        return HttpResponseRedirect("thank-you")
+    return render(request,"reviews/review.html")
+
+def thank_you(request):
+    return render(request,"reviews/thank_you.html")
